96f14811bd24d3acf36626bfef8082c710f994d0
Xewus <xewuss@yandex.ru>
2022-08-19 21:34:08 +0300

    cards for messages

--- pages/index.py
+++ pages/index.py
@@ -34,46 +34,50 @@ index = '''
                 </div>
               </form>
             </div>
-
             <div class="card-body" style="display: flex; justify-content: center">
-              <div id="messages">
-              </div>
             </div><!-- card-body -->
           </div><!-- card -->
+              
+              <div id="messages">
+              </div>
+
         </div><!-- col -->
       </main>
     </div><!-- bg-image -->
 
     <script>
-        var ws = new WebSocket("%8s");
-        ws.onmessage = function(event) {
-            var data = JSON.parse(event.data)
-            if (data.status == 400) {
-                alert(data.text)
-            } else if (data.status == 200) {
-                var messages = document.getElementById("messages")
-                var message = document.createElement('ol')
-                var content = document.createTextNode(data.num + ": " + data.text);
-                message.appendChild(content)
-                messages.appendChild(message)
-            } else {
-              alert("Неизвестная ошибка")
-            }
-        };
-
-        function sendMessage(event) {
-            if (ws.readyState === ws.OPEN) {
-                var input = document.getElementById("messageText")
-                var data = {
-                    "text": document.getElementById("messageText").value
-                };
-                ws.send(JSON.stringify(data));
-                input.value = ""
-                event.preventDefault()
-            } else {
-                alert("Нет соединения с сервером")
-            }
+      var ws = new WebSocket("%8s");
+      ws.onmessage = function(event) {
+        var data = JSON.parse(event.data)
+        if (data.status == 400) {
+          alert(data.text)
+        } else if (data.status == 200) {
+          var messages = document.getElementById("messages")
+          var card = document.createElement("message-card")
+          card.classList.add("card")
+          var message = document.createElement('ol')
+          var content = document.createTextNode(data.num + ": " + data.text);
+          message.appendChild(content)
+          card.appendChild(message)
+          messages.appendChild(card)
+        } else {
+          alert("Неизвестная ошибка")
         }
+      };
+
+      function sendMessage(event) {
+        if (ws.readyState === ws.OPEN) {
+          var input = document.getElementById("messageText")
+          var data = {
+            "text": document.getElementById("messageText").value
+          };
+          ws.send(JSON.stringify(data));
+          input.value = ""
+          event.preventDefault()
+        } else {
+          alert("Нет соединения с сервером")
+        }
+      };
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
